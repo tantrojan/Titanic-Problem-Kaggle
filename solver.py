c084391c14e7929981f21f04f317f729c7f38959
tantrojan <tg.99ghosh@gmail.com>
2018-05-31 02:56:45 +0530

    ACCURACY 0.60 using KNN classifier

--- solver.py
+++ solver.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing ,cross_validation,neighbors
 
-# Reading the csv to pandas dataframe
+# Reading the training csv to pandas dataframe
 df = pd.read_csv("./datasets/train.csv");
 
 # converting male/female to 1/0
@@ -17,18 +17,49 @@ df["Family"] = c
 df = df[["Pclass","Age","Family","Gender","Fare","Survived"]]
 
 # Dropping the Rows consisting of NaN
-df.dropna(inplace=True)
+df.fillna(0,inplace=True)
 
-# Features
+# Separating Features and label
 X = np.array(df.drop(['Survived'],1))
+y = np.array(df['Survived'])
 
-# Label
-y = np.array(df['class'])
+# Separating training and testing datas
+X_train,X_test,y_train,y_test = cross_validation.train_test_split(X,y,test_size=0.2)
 
-
-
+# Setting Up Classifier
 clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train,y_train)
 
+# Checking for accuracy
 accuracy = clf.score(X_test,y_test)
 print(accuracy)
+
+# Reading the file goes result is to be estimated
+df2 = pd.read_csv("./datasets/test.csv");
+ID = np.array(df2["PassengerId"])
+
+# converting male/female to 1/0
+df2["Gender"] = [ int(item=="male") for item in df2["Sex"].tolist()]
+
+# Counting no of family members
+sum_list = np.array(df2[["Parch","SibSp"]])
+c = [ sum(i) for i in sum_list ]
+df2["Family"] = c
+
+# Keeping the required Items only
+df2 = df2[["Pclass","Age","Family","Gender","Fare"]]
+df2.fillna(0,inplace=True)
+print(np.array(df2))
+
+
+prediction = clf.predict(np.array(df2))
+
+result = {
+	"PassengerId" :ID,
+	"Survived" : prediction
+}
+
+res_df = pd.DataFrame(result);
+res_df.set_index('PassengerId', inplace=True)
+res_df.to_csv("./final_res.csv")
+
